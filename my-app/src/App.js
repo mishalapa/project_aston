43867487c22d864aac43c8dd2fb69f3e3d647c33
mishalapa <mishamedme@gmail.com>
2022-04-14 11:25:33 +0300

    docs: add new project

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div>
+
+    </div>
+
+  );
+}
+
+export default App;
